c1dd56bff8a3ab726ec3b415fd2e7c16e706324e
Nonit Mittal <nonitmittal@gmail.com>
2021-11-27 17:51:13 +0530

    Added back button in OpenDesk Navbar

--- src/components/OpenDesk/Navbar.tsx
+++ src/components/OpenDesk/Navbar.tsx
@@ -1,17 +1,20 @@
-import { useEffect, useReducer, useState } from 'react';
-import { BellIcon as BellIconOutline } from '@heroicons/react/outline';
-import { BellIcon as BellIconSolid } from '@heroicons/react/solid';
-import { ChevronDownIcon } from '@heroicons/react/solid';
-import Blockies from 'react-blockies-image';
-import { useRouter } from 'next/router';
-import { useLoadingContext } from '../../context/Loading';
-import { useSignerContext } from '../../context/Signer';
+import { useEffect, useReducer, useState } from "react";
+import { BellIcon as BellIconOutline } from "@heroicons/react/outline";
+import {
+  ArrowNarrowLeftIcon,
+  BellIcon as BellIconSolid,
+} from "@heroicons/react/solid";
+import { ChevronDownIcon } from "@heroicons/react/solid";
+import Blockies from "react-blockies-image";
+import { useRouter } from "next/router";
+import { useLoadingContext } from "../../context/Loading";
+import { useSignerContext } from "../../context/Signer";
 
 interface Props {}
 
 function navbarReducer(state, action) {
   switch (action.type) {
-    case 'NOTIFICATION':
+    case "NOTIFICATION":
       return { search: false, notification: true };
     default:
       return { search: false, notification: false };
@@ -22,7 +25,7 @@ const Navbar = (props: Props) => {
   const router = useRouter();
   const { signer } = useSignerContext();
   const [signerAddress, setSignerAddress] = useState<string>(
-    '0x0000000000000000000000000000000000000000',
+    "0x0000000000000000000000000000000000000000"
   );
   const { setLoading } = useLoadingContext();
   const [navbarState, dispatch] = useReducer(navbarReducer, {
@@ -37,19 +40,29 @@ const Navbar = (props: Props) => {
   }, [signer]);
 
   return (
-    <nav className='navbar w-full text-neutral space-x-3 bg-white fixed pl-28 pr-5 py-4 z-10'>
-      <div className='flex-1 hidden lg:flex'>
-        <span className='text-accent text-3xl font-bold'>OpenDesk</span>
+    <nav className="navbar w-full text-neutral space-x-3 bg-white fixed pl-9 pr-5 py-4 z-10">
+      <p className="cursor-pointer pr-10">
+        <ArrowNarrowLeftIcon
+          className="w-6 h-6"
+          onClick={() => {
+            setLoading(true);
+            router.push("/");
+          }}
+        />
+      </p>
+      <div className="flex-1 hidden lg:flex">
+        <span className="text-accent text-3xl font-bold">OpenDesk</span>
       </div>
-      <div className='flex-1 lg:flex-none h-10'>
-        <div data-theme='light' className='form-control w-full'>
-          <div className='relative'>
+      <div className="flex-1 lg:flex-none h-10">
+        <div data-theme="light" className="form-control w-full">
+          <div className="relative">
             <button
-              className='btn btn-accent'
+              className="btn btn-accent"
               onClick={() => {
                 setLoading(true);
                 router.push(`OpenDesk/newbook`);
-              }}>
+              }}
+            >
               New Book
             </button>
           </div>
@@ -58,39 +71,40 @@ const Navbar = (props: Props) => {
 
       <div
         className={`flex-none ${
-          navbarState.notification && 'bg-green-100'
+          navbarState.notification && "bg-green-100"
         } rounded-lg`}
         onClick={() => {
-          dispatch({ type: 'NOTIFICATION' });
-        }}>
-        <button className='btn btn-ghost focus:outline-none'>
+          dispatch({ type: "NOTIFICATION" });
+        }}
+      >
+        <button className="btn btn-ghost focus:outline-none">
           {navbarState.notification ? (
-            <BellIconSolid className='h-5 w-5 text-accent' />
+            <BellIconSolid className="h-5 w-5 text-accent" />
           ) : (
-            <BellIconOutline className='h-5 w-5' />
+            <BellIconOutline className="h-5 w-5" />
           )}
         </button>
       </div>
 
-      <div className='flex-none bg-gray-100 w-2/12 rounded-lg cursor-pointer'>
-        <div className='avatar flex-none'>
-          <div className='rounded-tl-lg rounded-bl-lg w-12 h-12 bg-white object-fill'>
+      <div className="flex-none bg-gray-100 w-2/12 rounded-lg cursor-pointer">
+        <div className="avatar flex-none">
+          <div className="rounded-tl-lg rounded-bl-lg w-12 h-12 bg-white object-fill">
             <Blockies seed={signerAddress} size={48} scale={8} />
           </div>
         </div>
-        <div className='flex-1 ml-5'>
-          <div className='flex flex-col truncate'>
-            <p className='text-sm font-semibold'>Personal Wallet</p>
-            <p className='text-2xs'>
+        <div className="flex-1 ml-5">
+          <div className="flex flex-col truncate">
+            <p className="text-sm font-semibold">Personal Wallet</p>
+            <p className="text-2xs">
               {signer
                 ? `${signerAddress.slice(0, 8)}.....${signerAddress.slice(-4)}`
-                : '0x0000000000000000000000000000000000000000'}
+                : "0x0000000000000000000000000000000000000000"}
             </p>
           </div>
         </div>
-        <div className='flex-none mx-3'>
-          <span className='block leading-relaxed text-center'>
-            <ChevronDownIcon className='w-4 h-4' />
+        <div className="flex-none mx-3">
+          <span className="block leading-relaxed text-center">
+            <ChevronDownIcon className="w-4 h-4" />
           </span>
         </div>
       </div>
