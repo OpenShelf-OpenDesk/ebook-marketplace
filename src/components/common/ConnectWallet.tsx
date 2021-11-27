28366ffd9df9d0d4e03d97bc4d42e9c10b3b9b84
Raghav Goyal <12301raghavgoyal@gmail.com>
2021-11-27 18:26:35 +0530

    add provider events listeners

--- src/components/common/ConnectWallet.tsx
+++ src/components/common/ConnectWallet.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import Image from 'next/image';
-import { XIcon } from '@heroicons/react/solid';
-import { connectToWallet } from '../../controllers/ConnectWallet';
-import { useSignerContext } from '../../context/Signer';
-import { useLoadingContext } from '../../context/Loading';
+import React from "react";
+import Image from "next/image";
+import { XIcon } from "@heroicons/react/solid";
+import { connectToWallet } from "../../controllers/ConnectWallet";
+import { useSignerContext } from "../../context/Signer";
+import { useLoadingContext } from "../../context/Loading";
+import { useEffect } from "react";
 
 interface Props {}
 
@@ -11,48 +12,62 @@ const ConnectWallet = (props: Props) => {
   const { setSigner } = useSignerContext();
   const { setLoading } = useLoadingContext();
 
+  useEffect(() => {
+    return () => {
+      setLoading(true);
+    };
+  }, []);
+
   return (
-    <section className='fixed z-10 w-screen h-screen flex flex-row justify-center'>
-      <div className='bg-white h-2/4 w-1/4 self-center relative pt-5 rounded-lg shadow-md'>
-        <XIcon className='absolute w-5 h-5 right-4 top-4 cursor-pointer hover:text-secondary' />
-        <div className='w-full h-full flex flex-col justify-center space-y-10'>
-          <div className='flex flex-row justify-evenly'>
-            <h1 className='text-4xl font-bold text-primary '>OpenShelf</h1>
-            <h1 className='text-4xl font-bold text-accent '>OpenDesk</h1>
+    <section className="fixed z-10 w-screen h-screen flex flex-row justify-center">
+      <div className="bg-white h-2/4 w-1/4 self-center relative pt-5 rounded-lg shadow-md">
+        <XIcon className="absolute w-5 h-5 right-4 top-4 cursor-pointer hover:text-secondary" />
+        <div className="w-full h-full flex flex-col justify-center space-y-10">
+          <div className="flex flex-row justify-evenly">
+            <h1 className="text-4xl font-bold text-primary ">OpenShelf</h1>
+            <h1 className="text-4xl font-bold text-accent ">OpenDesk</h1>
           </div>
-          <div className='flex flex-col justify-center space-y-2 px-12 pt-5'>
-            <h3 className='text-center font-bold text-xl py-2 text-gray-600'>
+          <div className="flex flex-col justify-center space-y-2 px-12 pt-5">
+            <h3 className="text-center font-bold text-xl py-2 text-gray-600">
               Connect To Wallet
             </h3>
             <button
-              className='btn btn-outline border-gray-300 flex flex-row justify-between'
+              className="btn btn-outline border-gray-300 flex flex-row justify-between"
               onClick={async () => {
-                setLoading(true);
-                connectToWallet().then(async (_signer) => {
+                const providerEventsCB = async (_signer, _address) => {
+                  if (_signer && _address) {
+                    setSigner({ address: _address, signer: _signer });
+                  } else {
+                    setSigner(undefined);
+                  }
+                };
+                connectToWallet(providerEventsCB).then(async (_signer) => {
                   if (_signer) {
                     const _address = await _signer.getAddress();
                     setSigner({ address: _address, signer: _signer });
+                    setTimeout(() => setLoading(false), 500);
                   }
                 });
-              }}>
-              <span className='text-sm text-semibold'>Metamask</span>
+              }}
+            >
+              <span className="text-sm text-semibold">Metamask</span>
               <Image
-                src='/Metamask_Fox.svg'
+                src="/MetaMask_Fox.svg"
                 height={32}
                 width={32}
-                layout='fixed'
+                layout="fixed"
               />
             </button>
-            <button className='btn btn-outline border-gray-300 flex flex-row justify-between'>
-              <span className='text-sm text-semibold'>Brave Wallet</span>
+            <button className="btn btn-outline border-gray-300 flex flex-row justify-between">
+              <span className="text-sm text-semibold">Brave Wallet</span>
               <Image
-                src='/brave.svg'
+                src="/brave.svg"
                 height={38 * 0.85}
                 width={32 * 0.85}
-                layout='fixed'
+                layout="fixed"
               />
             </button>
-            <p className='pt-5 text-xs font-semibold text-center'>
+            <p className="pt-5 text-xs font-semibold text-center">
               By connecting your wallet you accept our Terms of Use and Privacy
               Policy.
             </p>
--- src/controllers/ConnectWallet.ts
+++ src/controllers/ConnectWallet.ts
@@ -1,10 +1,36 @@
-import Web3Modal from 'web3modal';
-import { ethers } from 'ethers';
+import Web3Modal from "web3modal";
+import { ethers } from "ethers";
 
-export async function connectToWallet() {
-  const we3modal = new Web3Modal();
-  const connection = await we3modal.connect();
+export async function connectToWallet(cb) {
+  const web3modal = new Web3Modal();
+  const connection = await web3modal.connect();
   const provider = new ethers.providers.Web3Provider(connection);
+
+  connection.on("accountsChanged", async (accounts: string[]) => {
+    if ((await provider.listAccounts()).length > 0) {
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      cb(signer, address);
+    } else {
+      cb(null, null);
+    }
+  });
+
+  // Subscribe to chainId change
+  connection.on("chainChanged", (chainId: number) => {
+    console.log(chainId);
+  });
+
+  // Subscribe to provider connection
+  connection.on("connect", (info: { chainId: number }) => {
+    console.log(info);
+  });
+
+  // Subscribe to provider disconnection
+  connection.on("disconnect", (error: { code: number; message: string }) => {
+    console.log(error);
+  });
+
   if ((await provider.listAccounts()).length > 0) {
     return provider.getSigner();
   }
