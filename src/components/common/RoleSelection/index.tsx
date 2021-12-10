6107d1ed492976308ba90d709f059aad04ec14cd
Nonit Mittal <nonitmittal@gmail.com>
2021-12-10 11:39:14 +0530

    updated the svg hover state

--- src/components/common/RoleSelection/index.tsx
+++ src/components/common/RoleSelection/index.tsx
@@ -1,8 +1,8 @@
-import { useRouter } from 'next/router';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useEffect } from 'react';
-import { useLoadingContext } from '../../../context/Loading';
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect } from "react";
+import { useLoadingContext } from "../../../context/Loading";
 
 interface Props {}
 
@@ -19,46 +19,46 @@ const RoleSelection = (props: Props) => {
     };
   }, []);
   return (
-    <section id='role-selection' className='hero min-h-screen max-h-screen'>
-      <div className='hero-content w-3/4 h-3/5'>
-        <div className='grid grid-cols-2 gap-x-20 w-full h-full'>
-          <div className='group flex flex-col card border border-gray-200 shadow-lg'>
-            <div className='flex h-64 justify-center relative'>
+    <section id="role-selection" className="hero min-h-screen max-h-screen">
+      <div className="hero-content w-3/4 h-3/5">
+        <div className="grid grid-cols-2 gap-x-20 w-full h-full">
+          <div className="group flex flex-col card border border-gray-200 shadow-lg">
+            <div className="flex h-64 justify-center relative">
               <Image
                 src={`/undraw_book_lover_mkck.svg`}
-                layout='fill'
-                className='scale-95 transition duration-500 ease-in-out transform group-hover:-translate-y-1 group-hover:scale-105'
+                layout="fill"
+                className="scale-95 transition duration-500 ease-in-out transform group-hover:-translate-y-1 group-hover:scale-90"
               />
             </div>
-            <div className='card-body text-gray-700'>
-              <div className='card-title text-2xl'>OpenShelf</div>
+            <div className="card-body text-gray-700">
+              <div className="card-title text-2xl">OpenShelf</div>
               <p>Fill your shelf with the bestselling classic books.</p>
-              <div className='flex flex-col justify-end h-full'>
-                <Link href='/OpenShelf'>
-                  <button className='btn btn-sm btn-primary self-end w-full'>
+              <div className="flex flex-col justify-end h-full">
+                <Link href="/OpenShelf">
+                  <button className="btn btn-sm btn-primary self-end w-full">
                     Reader
                   </button>
                 </Link>
               </div>
             </div>
           </div>
-          <div className='group flex flex-col card border border-gray-200 shadow-lg'>
-            <div className='flex h-64 justify-center relative'>
+          <div className="group flex flex-col card border border-gray-200 shadow-lg">
+            <div className="flex h-64 justify-center relative">
               <Image
                 src={`/undraw_studying_s3l7.svg`}
-                layout='fill'
-                className='transition duration-500 ease-in-out transform group-hover:-translate-y-1 group-hover:scale-110'
+                layout="fill"
+                className="transition duration-500 ease-in-out transform group-hover:-translate-y-1 group-hover:scale-90"
               />
             </div>
-            <div className='card-body text-gray-700'>
-              <div className='card-title text-2xl'>OpenDesk</div>
+            <div className="card-body text-gray-700">
+              <div className="card-title text-2xl">OpenDesk</div>
               <p>
                 Enlighten the world with your imagination, real stories or
                 research.
               </p>
-              <div className='flex flex-col justify-end h-full'>
-                <Link href='/OpenDesk'>
-                  <button className='btn btn-sm btn-accent self-end w-full'>
+              <div className="flex flex-col justify-end h-full">
+                <Link href="/OpenDesk">
+                  <button className="btn btn-sm btn-accent self-end w-full">
                     Author
                   </button>
                 </Link>
