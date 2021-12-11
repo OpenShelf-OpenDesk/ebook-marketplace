0355568fb081adebc595116caba10778ae40f5e8
Nonit Mittal <nonitmittal@gmail.com>
2021-12-11 19:34:47 +0530

    Added Desk in OpenDesk

--- pages/OpenDesk/index.tsx
+++ pages/OpenDesk/index.tsx
@@ -1,14 +1,14 @@
-import type { NextPage } from 'next';
-import { useState } from 'react';
-import Desk from '../../src/components/OpenDesk/Views/Desk';
-import Home from '../../src/components/OpenDesk/Views/Home';
+import type { NextPage } from "next";
+import { useState } from "react";
+import Desk from "../../src/components/OpenDesk/Views/Desk";
+import Home from "../../src/components/OpenDesk/Views/Home";
 
 const OpenDesk: NextPage = () => {
   const [selected, setSelected] = useState<1 | 2 | 3>(1);
   if (selected == 1) {
     return <Home selected={selected} setSelected={setSelected} />;
   } else if (selected == 2) {
-    return <Desk />;
+    return <Desk selected={selected} setSelected={setSelected} />;
   }
 };
 
